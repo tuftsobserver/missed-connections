20bedb4b10acf1bd6dd44261a11e5f3408dc8203
mimmackk <mimmackattack@gmail.com>
2017-02-25 11:56:43 -0500

    pulls new data from gsheets

--- js/index_script.js
+++ js/index_script.js
@@ -1,18 +1,18 @@
 $(document).ready(function() {
-	var colors = ['wedgewood','pistachio','rooster','heliotrope','festival','cosmos'];
     var url = "https://spreadsheets.google.com/feeds/list/19N2nlNo9brg1F5YsSewE4nQHZBhP77CU0Lv-aY_kT7w/1/public/values?alt=json"
+
+    // The following generates the HTML for each new box.
+    var bit1 = '<li><div class="box"><div class="box-title">';
+                // Insert title text here
+    var bit2 = '</div><div class="box-story">';
+                // Insert story text here
+    var bit3 = '</div></div></li>';
+
     $.getJSON(url, function(data) {
         var entry = data.feed.entry;
-        var i = 0;
         $(entry).each(function() {
-        	if (i == colors.length) i = 0;
-        	$('.container--cards').append('<div class="card--'+colors[i]+' box"><p class="card__headline">'+this.gsx$title.$t+'</p></div>');
-        	i++;
-        });
-        $(entry).each(function() {
-        	if (i == colors.length) i = 0;
-        	$('.container--cards').append('<div class="card--'+colors[i]+' box"><p class="card__headline">'+this.gsx$title.$t+'</p></div>');
-        	i++;
+        	toAdd = bit1 + this.gsx$title.$t + bit2 + this.gsx$story.$t + bit3;
+            document.getElementById("grid").innerHTML = toAdd + document.getElementById("grid").innerHTML;
         });
     });
 
